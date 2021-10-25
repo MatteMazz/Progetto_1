737770539dc28f830643fd7737aba2db0182fafd
A-P-M-MAZZETTO\Abstract <matteo.mazzetto@abstract.it>
2021-10-25 17:29:50 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 export default function App() {
   return (
     <Router>
-      <Route exact path="/" component={Prodotti} />
+      <Route exact path="/Progetto_1" component={Prodotti} />
       <Route exact path="/det/:UPC" component={Dettaglio} />
     </Router>
   );
