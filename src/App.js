106997516f1e15bef8a820c79049d71983bd2188
MatteMazz <92913815+MatteMazz@users.noreply.github.com>
2021-10-26 11:22:31 +0200

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <Router>
       <Route exact path="/det/:UPC" component={Dettaglio} />
-      <Route exact path="/Progetto_1" component={Prodotti} />
+      <Route exact path="/progetto_1" component={Prodotti} />
     </Router>
   );
 }
