9ce8c9e9eba440027b9c8b3315beb304157ff2ee
MatteMazz <92913815+MatteMazz@users.noreply.github.com>
2021-10-26 11:16:57 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 export default function App() {
   return (
     <Router>
-      <Route exact path="/Progetto_1" component={Prodotti} />
       <Route exact path="/det/:UPC" component={Dettaglio} />
+      <Route exact path="/Progetto_1" component={Prodotti} />
     </Router>
   );
 }
