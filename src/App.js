a9dc9907aaeb0a1a3cf132c5e2fc79fb97b69dec
MatteMazz <92913815+MatteMazz@users.noreply.github.com>
2021-10-26 14:28:16 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Dettaglio from "./components/Dettaglio";
 import Prodotti from "./components/Prodotti";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "@mui/material";
 
 export default function App() {
   return (
     <Router>
-      <Route exact path="/det/:UPC" component={Dettaglio} />
-      <Route exact path="/progetto_1" component={Prodotti} />
+      <Switch>
+        <Route path="/det/:UPC" component={Dettaglio} />
+        <Route path="/progetto_1" component={Prodotti} />
+      </Switch>
     </Router>
   );
 }
