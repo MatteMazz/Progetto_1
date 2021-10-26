59913b2efc5491d8e5da4ee37369ffc3155a94df
MatteMazz <92913815+MatteMazz@users.noreply.github.com>
2021-10-26 11:13:04 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import Dettaglio from "./components/Dettaglio";
 import Prodotti from "./components/Prodotti";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Route path="/det/:UPC" component={Dettaglio}></Route>
-      <Route exact path="/Progetto_1" component={Prodotti}></Route>
-    </BrowserRouter>
+    <Router>
+      <Route exact path="/Progetto_1" component={Prodotti} />
+      <Route exact path="/det/:UPC" component={Dettaglio} />
+    </Router>
   );
 }
-
-// <Router>
-//   <Route exact path="/det/:UPC" component={Dettaglio} />
-//   <Route exact path="/Progetto_1" component={Prodotti} />
-// </Router>
